126c0efbb254d4f931ac42a951367fd62657f63b
shivanit16 <76446420+shivanit16@users.noreply.github.com>
2021-06-20 16:57:03 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,63 +1,82 @@
 import requests
 import json
 import datetime
+import pytz
 
-def get_request():
-    rurl = 'https://api.cowin.gov.in/api/v1/reports/v2/getPublicReports?state_id=&district_id=&date=2021-06-15'
+IST = pytz.timezone('Asia/Kolkata')
+
+
+def get_request(pr1,pr2):
+    rurl = 'https://api.cowin.gov.in/api/v1/reports/v2/getPublicReports?state_id=&district_id=&date=%s' % (pr1)
     r = requests.get(rurl)
 
     if r.status_code == 200:
         cowin_data = r.json()
         BeneficiariesGroupBy = cowin_data.get('getBeneficiariesGroupBy')
-        data15 = BeneficiariesGroupBy[-1] 
-        needed_data15 = BeneficiariesGroupBy[-1]
-        del needed_data15['state_id']
-        del needed_data15['id']
-        del needed_data15['title']
-        del needed_data15['today']
-        del needed_data15['state_name']
-        del needed_data15['total']
+        yesterday = BeneficiariesGroupBy[30] 
+        print(yesterday)
+        needed_data2 = BeneficiariesGroupBy[30]
+        del needed_data2['state_id']
+        del needed_data2['id']
+        del needed_data2['title']
+        del needed_data2['today']
+        del needed_data2['state_name']
+        del needed_data2['total']
 
-    surl = 'https://api.cowin.gov.in/api/v1/reports/v2/getPublicReports?state_id=&district_id=&date=2021-06-14'
+    surl = 'https://api.cowin.gov.in/api/v1/reports/v2/getPublicReports?state_id=&district_id=&date=%s' % (pr2)
     s = requests.get(surl)
 
     if s.status_code == 200:
         cowin_data = s.json()
         BeneficiariesGroupBy = cowin_data.get('getBeneficiariesGroupBy')
-        data14 = BeneficiariesGroupBy[-1]
-        needed_data14 = BeneficiariesGroupBy[-1]
-        del needed_data14['state_id']
-        del needed_data14['id']
-        del needed_data14['title']
-        del needed_data14['today']
-        del needed_data14['state_name']
-        del needed_data14['total']
+        daybefore_yes = BeneficiariesGroupBy[30]
+        print(daybefore_yes)
+        needed_data1 = BeneficiariesGroupBy[30]
+        del needed_data1['state_id']
+        del needed_data1['id']
+        del needed_data1['title']
+        del needed_data1['today']
+        del needed_data1['state_name']
+        del needed_data1['total']
 
     # Using dictionary comprehension + keys()
     # Subtraction of dictionaries
-    res = {key: needed_data15[key] - needed_data14.get(key, 0) for key in needed_data15.keys()}
-    res.update({'state_name': 'Daman and Diu'})
+    res = {key: needed_data2[key] - needed_data1.get(key, 0) for key in needed_data2.keys()}
   
-    final_result(res)
+    final_result(res,pr1)
 
-def final_result(diff):
-    rurl = 'https://api.cowin.gov.in/api/v1/reports/v2/getPublicReports?state_id=&district_id=&date=2021-06-15'
+def final_result(diff,pr):
+    rurl = 'https://api.cowin.gov.in/api/v1/reports/v2/getPublicReports?state_id=&district_id=&date=%s' % (pr)
     r = requests.get(rurl)
 
     if r.status_code == 200:
         cowin_data = r.json()
         BeneficiariesGroupBy = cowin_data.get('getBeneficiariesGroupBy')
-        data15 = BeneficiariesGroupBy[-1] 
-        data15.update(diff)
-        del data15['state_id']
-        del data15['id']
-        del data15['title']
-        del data15['today']
-        final_data = data15
+        data2 = BeneficiariesGroupBy[30] 
+        data2.update(diff)
+        del data2['state_id']
+        del data2['id']
+        del data2['title']
+        del data2['today']
+        final_data = data2
         print(final_data)
 
     
 def main(): 
-    get_request()
+    Current_Date_Formatted = datetime.datetime.now(IST).strftime ('%Y-%m-%d') # format the date to ddmmyyyy
+    print ('Current Date: ' + str(Current_Date_Formatted))
+    
+    Previous_Date1 = datetime.datetime.now(IST) - datetime.timedelta(days=1)
+    Previous_Date_Formatted1 = Previous_Date1.strftime ('%Y-%m-%d') # format the date to ddmmyyyy
+    print ('Previous Date1: ' + str(Previous_Date_Formatted1))
+
+    Previous_Date2 = datetime.datetime.now(IST) - datetime.timedelta(days=2)
+    Previous_Date_Formatted2 = Previous_Date2.strftime ('%Y-%m-%d') # format the date to ddmmyyyy
+    print ('Previous Date2: ' + str(Previous_Date_Formatted2))
+
+
+
+    get_request(Previous_Date_Formatted1,Previous_Date_Formatted2)
+
 
 main()
